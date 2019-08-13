2273cd6e257c9f76509234c6a84f480860ddb9ec
Ben Davenport <benjamin.p.davenport@gmail.com>
2019-08-13 13:06:52 -0400

    Practice with Node and Postgres

---
+++ db.js
@@ -0,0 +1,13 @@
+// Import the dotenv module
+//call the config method
+require('dotenv').config();
+
+const pgp = require('pg-promise')();
+const db = pgp({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+});
+
+console.log('Yay you did it');
+console.log(db);
---
+++ schema.sql
@@ -0,0 +1,7 @@
+create table todos(
+    id serial primary key,
+    priority integer not null,
+    task varchar(100) not null,
+    status boolean default false
+);
+
