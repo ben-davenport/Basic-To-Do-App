b69a4c297ee34b5eaee4fc688804c0c49e31a1eb
Ben Davenport <benjamin.p.davenport@gmail.com>
2019-08-15 09:47:04 -0400

    Update schema and seed with users

--- seed.sql
+++ seed.sql
@@ -1,4 +1,20 @@
+insert into users
+    (displayname, username)
+VALUES
+    ('alice', 'l33th4x0r'),
+    ('bob', 'puppy_lover')
+;
+
 insert into todos 
-    (priority, task)
+    (priority, task, user_id)
 values
-    (1, 'feed the cat');
+    (1, 'feed the cat', 1),
+    (2, 'pet the cat', 1),
+    (3, 'worship the cat', 1),
+    (10, 'go the work', 1),
+    (1, 'feed the dog', 2),
+    (2, 'pet the dog', 2),
+    (3, 'worship the dog', 2)
+;
+
+select * from todos;
