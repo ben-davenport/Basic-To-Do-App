ff4d2a45d4841a6116bdd3234038be8d1699ed02
Ben Davenport <benjamin.p.davenport@gmail.com>
2019-08-15 14:46:56 -0400

    added user post including body parser

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const User = require(`./models/User`)
 
 //Create the server and call it "app"
 const app = express();
+
+//use the urlencoded middleware
+//to read POST bodies
+app.use(express.urlencoded({extended: true}));
+
 //create a variable for the port #
 const port = 4000;
 
@@ -13,46 +18,66 @@ const port = 4000;
 
 // const server = http.createServer((req, res) => {
     //replace with app.get()
-app.get(`/todos`,(req, res) => {
+app.get(`/todos`,async (req, res) => {
     console.log(`You've got a request`);
-    const allTodos = Todo.getAll();
-    allTodos
-        .then((data) => {
-        console.log('Here is the data:');
-        console.log(data);
-        // res.end(JSON.stringify(data))
-        res.json(data);
-        })
+    const allTodos = await Todo.getAll();
+    res.json(allTodos);
+    // allTodos
+    //     .then((data) => {
+    //     console.log('Here is the data:');
+    //     console.log(data);
+    //     // res.end(JSON.stringify(data))
+    //     res.json(data);
+    //     })
 });
 
-app.get(`/todos/:taskId`,(req,res) => {    
+app.get(`/todos/:taskId`, async (req,res) => {    
     console.log("You asked for a specific task");
     console.log(req.params.taskId);
     //convert the route paramter to a number and make sure that it's in base 10
     const theId = parseInt(req.params.taskId, 10);
-    const oneTodo = Todo.getOne(theId);
-        oneTodo
-        .then((data) => {
-        // console.log(data);
-        // res.end(JSON.stringify(data))
-        res.json(data);
-        })
+    const oneTodo = await Todo.getOne(theId);
+    res.json(oneTodo);
+        // oneTodo
+        // .then((data) => {
+        // // console.log(data);
+        // // res.end(JSON.stringify(data))
+        // res.json(data);
+        // })
 });
 
-app.get(`/users`,(req,res)=>{
-    const allUsers = User.getAll();
-    allUsers
-        .then((data) => {
-            res.json(data)
-        });
+app.get(`/users`, async (req,res)=>{
+    const allUsers = await User.getAll();
+    res.json(allUsers);
+    // allUsers
+    //     .then((data) => {
+    //         res.json(data)
+    //     });
 });
-app.get(`/users/:userId`, (req,res)=>{
+app.get(`/users/:userId`, async (req,res)=>{
     const theId = parseInt(req.params.userId, 10);
-    const aUser = User.getOne(theId);
-    aUser
-        .then((data)=>{
-            res.json(data);
-        })
+    const aUser = await User.getOne(theId);
+    res.json(aUser);
+    // aUser
+    //     .then((data)=>{
+    //         res.json(data);
+    //     })
+})
+
+// User.createUser({
+//     displayname: "whowho",
+//     username: "who2who2"
+// })
+
+app.post(`/users`, async (req, res) => {
+    console.log("We got a post request!");
+    res.send("good job")
+    console.log('Here is the body:');
+    console.log(req.body)
+    const newUserInfo = await User.createUser({
+        displayname: "jonathan",
+        username: "alwaysRight"
+    });
 })
 
 app.listen(port)
--- models/User.js
+++ models/User.js
@@ -31,7 +31,31 @@ async function getOne(id){
     }
     };
 
+// Accept an argument of an object. That way we have flexibility later on.
+// That way we can add more database columns without having to update all
+// of our function calls.
+
+//Called below is the longhand version of the following shorthand, fully destructured version
+//When called they only pass in an object,
+//they don't have to know that they are destructuring
+// async function createUser({displayname, username}){
+//     db.one(
+//         'INSERT INTO users (displayname,username) values ($1, $2)', 
+//         [displayname, username]);
+async function createUser(userDataObj){
+    const {displayname, username} = userDataObj;
+    const id = await db.one(`
+        INSERT INTO users (displayname,username) values ($1, $2)
+        
+        returning id`
+
+        ,[displayname, username]);
+    console.log(id);
+    return id;
+}
+
 module.exports = {
     getAll,
-    getOne
+    getOne,
+    createUser
 }
