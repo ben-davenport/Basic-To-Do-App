a63e44ef1dc95403284eeeca11a82c262c06464c
Ben Davenport <benjamin.p.davenport@gmail.com>
2019-08-15 11:52:22 -0400

    Updated to async functions for todo and users

--- models/Todo.js
+++ models/Todo.js
@@ -1,32 +1,52 @@
 const db = require(`../db`)
 
-function getAll(){
-    return db.any(`SELECT * FROM todos;`)
-    // .then((data) => {
+async function getAll(){
+    try {
+        const allTodos = await db.any(`SELECT * FROM todos;`)
+        return allTodos;}
+    catch(error){
+        console.log(`uh oh`)
+        console.log(error)
+        return [];
+    }
+}
+
+// This is the original way to do it with .then and not with async/await 
+    //.then((data) => {
     //     console.log('here is the data:');
     //     console.log(data);
     //     })
-    .catch((err) => {
-        console.log(`uh oh`)
-        console.log(err)
-    })
-}
+    // .catch((err) => {
+    //     console.log(`uh oh`)
+    //     console.log(err)
+    // })
 
 //When you want one and ony one, use the ".one()" method instead of .any()
 //That way when you don't find it, the .catch is triggered. 
 //This is better than doing an if-else inside of the .then()
-function getOne(id){
-    console.log(`GetOne's ID: ${id}`)
-    return db.one(`SELECT * FROM todos WHERE id=$1;`, [id])
-        // .then((data) => {
-        //     // console.log('here is the data:');
-        //     // console.log(data);
-        // })
-        .catch((err) => {
-            console.log(`uh oh`)
-            console.log(err)
-        })
+async function getOne(id){
+    try {
+        console.log(`GetOne's ID: ${id}`)
+        const oneTodo = await db.one(`SELECT * FROM todos WHERE id=$1;`, [id])
+        console.log('Here is one Todo')
+        return oneTodo;}
+    catch(error){
+        console.log(`uh oh`)
+        console.log(error)
+        return {};
+    }
 }
+
+// This is the original way to do it with .then and not with async/await 
+// return db.one(`SELECT * FROM todos WHERE id=$1;`, [id])
+//     // .then((data) => {
+//     //     // console.log('here is the data:');
+//     //     // console.log(data);
+//     // })
+//     .catch((err) => {
+//         console.log(`uh oh`)
+//         console.log(err)
+//     })
 
 module.exports = {
     //if you omit the colon and the repeated word, it assumes the same name
--- models/User.js
+++ models/User.js
@@ -1,23 +1,35 @@
 const db = require(`../db`);
 
 
-function getAll(){
-    return db.any(
-        'select * from users')
-    .catch((err)=>{
-        console.log(`Error getting users.`);
-        console.log(err)
-    })
+async function getAll(){
+    try{
+    // const allUsers = await db.any('select * from users')
+    // return allUsers
+    return await db.any('select * from users')
+    }
+    catch(error){
+        console.log("There was an error retrieving all users")
+        console.log(error);
+        return [];
+    }
 };
 
-function getOne(id){
-    return db.one(
-        'select * from users WHERE id=$1', [id])
-    .catch((err) =>{
-        console.log(`Error getting one user.`)
-        console.log(err)
-    })
-};
+async function getOne(id){
+    try {
+    const user = await db.one('select * from users WHERE id=$1', [id]);
+    const todosForUser = await db.any('select * from todos where user_id=$1', [id]);
+    user.todos = todosForUser;
+    return user
+    }
+    catch(error){
+        console.log("There was an error retrieving user")
+        console.log(error)
+        return{
+            id: 0,
+            displayname: 'No user found'
+        };
+    }
+    };
 
 module.exports = {
     getAll,
